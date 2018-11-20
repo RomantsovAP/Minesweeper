1041de1f4d4a89ae4182bc04ba1ec5053b6b9f75
Aleksey Romantsov <AlekseyRomantsov@yandex.ru>
2018-11-20 23:04:29 +0300

    add drawing line

--- src/main/java/JavaSweeper.java
+++ src/main/java/JavaSweeper.java
@@ -18,7 +18,10 @@ public class JavaSweeper extends JFrame {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.drawLine(0,0,500,300);
+                g.drawImage(getImage("bomb"), 0,0, this);
+                g.drawImage(getImage("num1"), 50,0, this);
+                g.drawImage(getImage("num1"), 50,50, this);
+                g.drawImage(getImage("num1"), 0,50, this);
             }
         };
         panel.setPreferredSize(new Dimension(500, 300));
@@ -32,6 +35,11 @@ public class JavaSweeper extends JFrame {
         this.setResizable(false );
         this.pack();
         this.setLocationRelativeTo(null);
+    }
+
+    private Image getImage(String name) {
+        ImageIcon icon = new ImageIcon("src/main/resources/img/" + name + ".png");
+        return icon.getImage();
     }
 
 }
