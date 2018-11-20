f992d2685df6a72c5104bb35bb0e24c3b4db0065
Aleksey Romantsov <AlekseyRomantsov@yandex.ru>
2018-11-20 23:15:31 +0300

    add enum for images

--- src/main/java/JavaSweeper.java
+++ src/main/java/JavaSweeper.java
@@ -4,6 +4,10 @@ import java.awt.*;
 public class JavaSweeper extends JFrame {
     private JPanel panel;
 
+    private final int COLS = 15;
+    private final int ROWS = 1;
+    private final int IMAGE_SIZE = 50;
+
     public static void main(String[] args) {
         new JavaSweeper();
     }
@@ -19,12 +23,12 @@ public class JavaSweeper extends JFrame {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 g.drawImage(getImage("bomb"), 0,0, this);
-                g.drawImage(getImage("num1"), 50,0, this);
-                g.drawImage(getImage("num1"), 50,50, this);
-                g.drawImage(getImage("num1"), 0,50, this);
+                g.drawImage(getImage("num1"), IMAGE_SIZE,0, this);
+
             }
         };
-        panel.setPreferredSize(new Dimension(500, 300));
+        panel.setPreferredSize(new Dimension(COLS * IMAGE_SIZE,
+                                            ROWS * IMAGE_SIZE));
         this.add(panel);
     }
 
